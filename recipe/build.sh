2b3a19c843df3026d1c9f2ee5831170eaf897476
Benjamin Bertrand <beenje@gmail.com>
2022-09-30 23:19:29 +0200

    Build version 9.4.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,24 @@
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_VERBOSE_MAKEFILE=ON \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCPPZMQ_BASE=$PREFIX \
-      -DIDL_BASE=$PREFIX \
-      -DOMNI_BASE=$PREFIX \
-      -DZMQ_BASE=$PREFIX \
-      -DTANGO_JPEG_MMX=OFF \
+      -DTANGO_CPPZMQ_BASE="$PREFIX" \
+      -DTANGO_IDL_BASE="$PREFIX" \
+      -DTANGO_OMNI_BASE="$PREFIX" \
+      -DTANGO_ZMQ_BASE="$PREFIX" \
+      -DTANGO_JPEG_BASE="$PREFIX" \
       -DBUILD_TESTING=OFF \
       ..
 
 make -j $CPU_COUNT
 make install
 
-# Separate debugging symbols
-${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION} ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
-chmod 664 ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
-${OBJCOPY} --strip-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION}
-${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg ${PREFIX}/lib/libtango.so.${PKG_VERSION}
+# Separate debugging symbols on Linux
+if [ ! -z "${OBJCOPY}" ]
+then
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION} ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
+  chmod 664 ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION}
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg ${PREFIX}/lib/libtango.so.${PKG_VERSION}
+fi
