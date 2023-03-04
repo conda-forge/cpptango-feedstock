fffdf182e583a5741f76a7e01b0446d2358e3d5f
Benjamin Bertrand <beenje@gmail.com>
2023-03-04 10:23:36 +0100

    Build version 9.3.6-rc2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,12 @@
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_VERBOSE_MAKEFILE=ON \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCPPZMQ_BASE=$PREFIX \
-      -DIDL_BASE=$PREFIX \
-      -DOMNI_BASE=$PREFIX \
-      -DZMQ_BASE=$PREFIX \
+      -DCPPZMQ_BASE="$PREFIX" \
+      -DIDL_BASE="$PREFIX" \
+      -DOMNI_BASE="$PREFIX" \
+      -DZMQ_BASE="$PREFIX" \
       -DTANGO_JPEG_MMX=OFF \
       -DBUILD_TESTING=OFF \
       ..
@@ -17,10 +16,13 @@ make install
 
 # LIBVERSION can't be set to PKG_VERSION for dev and rc releases
 # LIBVERSION=${PKG_VERSION}
-LIBVERSION=9.3.5
+LIBVERSION=9.3.6
 
-# Separate debugging symbols
-${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libtango.so.${LIBVERSION} ${PREFIX}/lib/libtango.so.${LIBVERSION}.dbg
-chmod 664 ${PREFIX}/lib/libtango.so.${LIBVERSION}.dbg
-${OBJCOPY} --strip-debug ${PREFIX}/lib/libtango.so.${LIBVERSION}
-${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libtango.so.${LIBVERSION}.dbg ${PREFIX}/lib/libtango.so.${LIBVERSION}
+# Separate debugging symbols on Linux
+if [ ! -z "${OBJCOPY}" ]
+then
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libtango.so.${LIBVERSION} ${PREFIX}/lib/libtango.so.${LIBVERSION}.dbg
+  chmod 664 ${PREFIX}/lib/libtango.so.${LIBVERSION}.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/lib/libtango.so.${LIBVERSION}
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libtango.so.${LIBVERSION}.dbg ${PREFIX}/lib/libtango.so.${LIBVERSION}
+fi
