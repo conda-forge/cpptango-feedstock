a4b26b5a51cbc1599034e0a70ef082eb28c707e7
Benjamin Bertrand <beenje@gmail.com>
2023-04-03 08:52:45 +0200

    version 9.3.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,8 @@
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_VERBOSE_MAKEFILE=ON \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCPPZMQ_BASE=$PREFIX \
       -DIDL_BASE=$PREFIX \
       -DOMNI_BASE=$PREFIX \
@@ -15,8 +14,11 @@ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 make -j $CPU_COUNT
 make install
 
-# Separate debugging symbols
-${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION} ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
-chmod 664 ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
-${OBJCOPY} --strip-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION}
-${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg ${PREFIX}/lib/libtango.so.${PKG_VERSION}
+# Separate debugging symbols on Linux
+if [ ! -z "${OBJCOPY}" ]
+then
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION} ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
+  chmod 664 ${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/lib/libtango.so.${PKG_VERSION}
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libtango.so.${PKG_VERSION}.dbg ${PREFIX}/lib/libtango.so.${PKG_VERSION}
+fi
